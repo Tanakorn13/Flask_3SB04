29ae633fba7deb5c826e077056c652c69b2d4410
Tanakorn13 <96484902+Tanakorn13@users.noreply.github.com>
2022-12-30 22:19:57 +0700

    template inherit, template

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask,render_template,redirect, url_for, request
+from flask_wtf import Form 
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length, AnyOf
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
@@ -12,6 +16,10 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/template_in')
+def template_in ():
+    return render_template('template_in.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000', debug=True)
